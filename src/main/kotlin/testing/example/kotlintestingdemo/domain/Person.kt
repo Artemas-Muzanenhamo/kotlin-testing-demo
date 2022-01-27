5ce2dd7cf2f002a93bf82e362d5e0ab722ba272d
Artemas-Muzanenhamo <artemasseng@gmail.com>
2022-01-27 22:04:03 +0000

    Add Person Service Test

--- src/main/kotlin/testing/example/kotlintestingdemo/domain/Person.kt
+++ src/main/kotlin/testing/example/kotlintestingdemo/domain/Person.kt
@@ -1,10 +1,5 @@
 package testing.example.kotlintestingdemo.domain
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
-import javax.persistence.Table
-
 data class Person(
     val id: Long? = null,
     val name: String? = null,
--- src/main/kotlin/testing/example/kotlintestingdemo/service/PersonService.kt
+++ src/main/kotlin/testing/example/kotlintestingdemo/service/PersonService.kt
@@ -2,11 +2,23 @@ package testing.example.kotlintestingdemo.service
 
 import org.springframework.stereotype.Service
 import testing.example.kotlintestingdemo.domain.Person
+import testing.example.kotlintestingdemo.entity.PersonEntity
+import testing.example.kotlintestingdemo.repository.PersonRepository
 
 @Service
-class PersonService {
+class PersonService(
+    val personRepository: PersonRepository
+) {
     fun create(person: Person) {
-        TODO("Not yet implemented")
+        personRepository.save(person.toEntity())
+    }
+
+    fun Person.toEntity(): PersonEntity {
+        return PersonEntity(
+            id = this.id,
+            name = this.name,
+            surname = this.surname
+        )
     }
 
 }
