48544528edfb99e19b4444d971b42c523afafc55
Artemas-Muzanenhamo <artemasseng@gmail.com>
2022-01-28 09:13:49 +0000

    Add a mockito kotlin test

--- src/test/kotlin/testing/example/kotlintestingdemo/service/AnotherPersonServiceTest.kt
+++ src/test/kotlin/testing/example/kotlintestingdemo/service/AnotherPersonServiceTest.kt
@@ -1,5 +1,6 @@
 package testing.example.kotlintestingdemo.service
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.mockito.BDDMockito.any
@@ -11,8 +12,10 @@ import testing.example.kotlintestingdemo.repository.PersonRepository
 
 @DisplayName("Mockito Kotlin Based Unit Test")
 class AnotherPersonServiceTest {
-    private val personRepository = mock<PersonRepository>()
-    private val personService = PersonService(personRepository)
+    private val personRepositoryMock = mock<PersonRepository>() {
+        on {findFirstByName("jason")}.thenReturn(PersonEntity(4324L, "jason", "kidd"))
+    }
+    private val personService = PersonService(personRepositoryMock)
 
     @Test
     fun `Should call the PersonRepository given a Person`() {
@@ -23,6 +26,23 @@ class AnotherPersonServiceTest {
         personService.create(person)
 
         // then
-        verify(personRepository).save(any(PersonEntity::class.java))
+        verify(personRepositoryMock).save(any(PersonEntity::class.java))
+    }
+
+    @Test
+    fun `Should Retrieve A Person From The PersonRepository By The Given Name`() {
+        // when
+        val person = personService.retrievePersonByName("jason")
+
+        // then
+        assertThat(person)
+            .isNotNull
+            .extracting {
+                assertThat(it.id).isEqualTo(4324L)
+                assertThat(it.name).isEqualTo("jason")
+                assertThat(it.surname).isEqualTo("kidd")
+            }
+
+        verify(personRepositoryMock).findFirstByName("jason")
     }
 }
